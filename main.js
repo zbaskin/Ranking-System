e4ea29d560a73cdc913b3ecd614a287120fcca5e
Zach Baskin <zbaskin@gatech.edu>
2023-01-19 01:29:26 -0500

    added ranking system logic container

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("hello");
+function getTwoRandInts(max) {
+    var rand1 = Math.floor(Math.random() * max);
+    var rand2 = rand1;
+    while (rand1 == rand2) {
+        rand2 = Math.floor(Math.random() * max);
+    }
+    return [rand1, rand2];
+}
+
+const rankingSystem = function (items) {
+    if (items.length < 3) {
+        return ["Error"];
+    }
+    const score = Array(items.length).fill(0);
+    var ranking = true;
+    while (ranking) {
+        var rands = getTwoRandInts(items.length);
+
+        // INSERT COMPARISON LOGIC
+        
+        ranking = false;
+    }
+    return score;
+}
+
+var films = ["Iron Man", "The Incredible Hulk", "Iron Man 2", "Thor", "Captain America: The First Avenger", "The Avengers"];
+var ranks = rankingSystem(films);
+console.log(ranks);
