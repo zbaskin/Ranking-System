7bc32527cea6dc656dffa5f10bae6afdad9e4eb1
Zach Baskin <zbaskin@gatech.edu>
2023-01-19 17:03:44 -0500

    segmented array creation and improved update function

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+function createArray(length) {
+    var arr = [];
+    for (let i = 0; i < length; i++) {
+        arr.push([0,0]);
+    }
+    return arr;
+}
+
 function getTwoRandInts(max) {
     var rand1 = Math.floor(Math.random() * max);
     var rand2 = rand1;
@@ -8,15 +16,24 @@ function getTwoRandInts(max) {
 }
 
 function updateWinner(items, winner, loser) {
-    items[winner]++;
-    items[loser]--;
+    items[winner][1]++;
+    items[loser][1]++;
+
+    items[winner][0] += (1 / items[winner][1]);
+    items[loser][0] -= (1 / items[loser][1]);
+
+    /*
+        ALT METHOD
+    items[winner][0] = (1 + items[winner][0]) / items[winner][1];
+    items[loser][0] = (items[loser][0] - 1) / items[loser][1];
+    */
 }
 
 const rankingSystem = function (items) {
     if (items.length < 3) {
         return ["Error"];
     }
-    const score = Array(items.length).fill(0);
+    const score = createArray(items.length);
     var ranking = true;
     while (ranking) {
         var rands = getTwoRandInts(items.length);
