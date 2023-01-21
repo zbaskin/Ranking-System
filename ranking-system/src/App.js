aae556d01cd11ece3dc2c6b0e75ffbdfc591479c
Zach Baskin <zbaskin@gatech.edu>
2023-01-21 18:41:02 -0500

    started react app work

--- ranking-system/src/App.js
+++ ranking-system/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button id="option1">Iron Man</button>
+        <button id="option2">The Avengers</button>
       </header>
     </div>
   );
